ec50f75447bfbe125bf4fe84cf71b207f57e3df2
Yujing Zheng <yujingz@me.com>
2014-09-07 03:07:32 -0500

    recent tweets

--- lib/Twilt/Perl.pm
+++ lib/Twilt/Perl.pm
@@ -1,25 +1,58 @@
 package Twilt::Perl;
+use Net::Twitter;
 use Dancer ':syntax';
 use Dancer::Plugin::Auth::Twitter;
+use Data::Dumper;
 
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
 
 auth_twitter_init();
 
 before sub {
-    return if request->path eq "/" or request->path =~ m{/auth/twitter/callback};
+  return if request->path eq "/" or request->path =~ m{/auth/twitter/callback};
 
-    if (not session('twitter_user')) {
-        redirect auth_twitter_authorize_url();
-    }
-};
-
-get '/home' => sub {
-  template 'home.tt'
+  if (not session('twitter_user')) {
+    redirect auth_twitter_authorize_url();
+  }
 };
 
 get '/' => sub {
   template 'landing.tt'
+};
+
+get '/home' => sub {
+  template 'home.tt' => {
+    name => session('twitter_user')->{"name"}
+  };
+};
+
+get '/twilt' => sub {
+  my $tweet_count = 20;
+
+  if (length(params->{'solo_twitter_username'})) {
+    my @results = ();
+    my $timeline = twitter->user_timeline({screen_name => "subdigital", count => $tweet_count});
+
+    my @results = ();
+    for my $tweet ( @$timeline ) {
+      my @item = ();
+      if ( $tweet->{retweeted_status} ) {
+        @item = ( $tweet->{user}{screen_name}, $tweet->{retweeted_status}{text}, $tweet->{retweeted_status}{created_at});
+      } else {
+        @item = ( $tweet->{user}{screen_name}, $tweet->{text}, $tweet->{created_at});
+      }
+
+      push @results, \@item;
+    }
+
+    template 'results.tt' => {
+      tweets => \@results
+    };
+  } elsif (length(params->{'common_twitter_username_a'}) && length(params->{'common_twitter_username_b'})){
+    return "Hello Common";
+  } else {
+    return 'Nothing';
+  }
 };
 
 true;
